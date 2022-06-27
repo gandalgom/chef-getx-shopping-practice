263257205475854d450725afa91e56e097941d0f
Max Yoo <devbear74@gmail.com>
2022-06-28 00:32:31 +0900

    스크린 UI 기본 구현

--- lib/view/shopping_page.dart
+++ lib/view/shopping_page.dart
@@ -5,10 +5,55 @@ class ShoppingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.red,
-      body: Center(
-        child: Text('Shopping Page'),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(12.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text('Product Name',
+                                  style: TextStyle(fontSize: 24.0)
+                                ),
+                                Text('Product Name'),
+                              ],
+                            ),
+                            const Text('\$30',
+                              style: TextStyle(fontSize: 24.0)
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('add to card'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 30.0),
+          const Text(
+            'Total amount',
+            style: TextStyle(fontSize: 25.0, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
