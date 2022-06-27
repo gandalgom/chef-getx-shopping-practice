ba2457fd19250c6387adfbefb68e88607614b2c0
Max Yoo <devbear74@gmail.com>
2022-06-28 01:03:03 +0900

    UI와 컨트롤러 연동 및 데이터 일부 수정

--- lib/controller/shopping_controller.dart
+++ lib/controller/shopping_controller.dart
@@ -17,30 +17,30 @@ class ShoppingController extends GetxController {
       Product(
         id: 1,
         price: 30,
-        description: 'some description about product',
+        description: 'some description about mouse',
         name: 'Mouse'
       ),
       Product(
         id: 2,
         price: 40,
-        description: 'some description about product',
+        description: 'some description about keyboard',
         name: 'Keyboard'
       ),
       Product(
         id: 3,
         price: 620,
-        description: 'some description about product',
+        description: 'some description about monitor',
         name: 'Monitor',
       ),
       Product(
         id: 4,
         price: 80,
-        description: 'some description about product',
-        name: 'Ram'),
+        description: 'some description about RAM',
+        name: 'RAM'),
       Product(
         id: 5,
         price: 120.5,
-        description: 'some description about product',
+        description: 'some description about speaker',
         name: 'Speaker',
       ),
     ];
--- lib/view/shopping_page.dart
+++ lib/view/shopping_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_x_practice/controller/shopping_controller.dart';
 
 class ShoppingPage extends StatelessWidget {
-  const ShoppingPage({Key? key}) : super(key: key);
+  ShoppingPage({Key? key}) : super(key: key);
+
+  final shoppingController = Get.put(ShoppingController());
 
   @override
   Widget build(BuildContext context) {
@@ -10,42 +14,50 @@ class ShoppingPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.all(12.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: GetX<ShoppingController>(
+              builder: (controller) {
+                return ListView.builder(
+                  itemCount: controller.products.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(12.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text('Product Name',
-                                  style: TextStyle(fontSize: 24.0)
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      controller.products[index].name,
+                                      style: const TextStyle(fontSize: 24.0)
+                                    ),
+                                    Text(
+                                      controller.products[index].description
+                                    ),
+                                  ],
                                 ),
-                                Text('Product Name'),
+                                Text(
+                                  '\$${controller.products[index].price}',
+                                  style: const TextStyle(fontSize: 24.0)
+                                ),
                               ],
                             ),
-                            const Text('\$30',
-                              style: TextStyle(fontSize: 24.0)
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: const Text('add to card'),
                             ),
                           ],
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('add to card'),
-                        ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 );
-              },
+              }
             ),
           ),
           const SizedBox(height: 30.0),
